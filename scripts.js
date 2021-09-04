72dd21269bae6aacf113ea32f99c83344f84f9a5
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-09-04 21:23:37 +0300

    Cleanup

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-const addHoursToDate = (objDate, intHours) => {
-    const numberOfMlSeconds = objDate.getTime();
-    const addMlSeconds = (intHours * 60) * 60 * 1000;
-    const newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
- 
-    return newDateObj;
-}
-
-const parseHoursMinutes = (timeAsString) => {
-    const timeSplit = timeAsString.split(".")
-    const hours = parseInt(timeSplit[0])
-    const minutes = parseInt(timeSplit[1])
-    return [hours ? hours : 0, minutes ? minutes : 0]
-}
-
 class TimeValue {
     hours
     minutes
@@ -82,7 +67,7 @@ class TimeValue {
 }
 
 const decideWhichFieldToCalculate = (startTime, breakDuration, endTime, workHours) => {
-    const numberOfNans = 1
+    // TODO: implement
 }
 
 const startTime = new TimeValue(null, null)
@@ -113,40 +98,16 @@ const calculateMissingTime = () => {
         console.log(workHours)
         document.getElementById("work-hours").value = workHours.asString()
     }
-
-    //calculateEndTime([startTimeHours, startTimeMinutes], breakDuration, [workHours, workMinutes])
-
-
-}
-
-const calculateEndTime = (startTime, breakDuration, workTime) => {
-    const date = new Date()
-    console.log(date)
-    date.setHours(startTime[0])
-    date.setMinutes(startTime[1])
-    date.setSeconds(0)
-    console.log(date)
-    const interimDate = addHoursToDate(date, breakDuration / 60)
-    console.log(interimDate)
-    const finalDate = addHoursToDate(interimDate, workTime[0] + (workTime[1] / 60))
-    console.log(finalDate)
-
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded")
 
     document.getElementById("calculate-time-button").addEventListener("click", (event) => {
-        // console.log(event)
         event.preventDefault()
         calculateMissingTime()
     })
 
-    /*
-    document.getElementById("start-time").addEventListener("change", event => {
-        console.log(event)
-    })
-*/
     Array.from(document.getElementsByClassName("watch-change")).forEach(item => {
         item.addEventListener("keyup", () => calculateMissingTime())
         item.addEventListener("change", () => calculateMissingTime())
