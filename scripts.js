93ac2c4752756959a450e0cb02dc8ed9626bab29
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-09-04 20:42:39 +0300

    Some methods for TimeValue

--- scripts.js
+++ scripts.js
@@ -11,13 +11,91 @@ const parseHoursMinutes = (timeAsString) => {
     const hours = parseInt(timeSplit[0])
     const minutes = parseInt(timeSplit[1])
     return [hours ? hours : 0, minutes ? minutes : 0]
+}
+
+class TimeValue {
+    hours
+    minutes
+    lastModifiedTimestamp
+    constructor(hours, minutes){
+        this.hours = hours + (parseInt(minutes / 60))
+        this.minutes = minutes % 60
+        this.lastModifiedTimestamp = Date.now()
+    }
+
+    asHours(){
+        return this.hours + (this.minutes / 60)
+    }
+
+    asMinutes(){
+        return (this.hours * 60) + this.minutes
+    }
+
+    add(otherTimeValue){
+        const totalMinutes = this.asMinutes() + otherTimeValue.asMinutes()
+        const newHours = parseInt(totalMinutes / 60)
+        const newMinutes = totalMinutes % 60
+        return new TimeValue(newHours, newMinutes)        
+    }
+
+    subtract(otherTimeValue){
+        const totalMinutes = this.asMinutes() - otherTimeValue.asMinutes()
+        const newHours = parseInt(totalMinutes / 60)
+        const newMinutes = totalMinutes % 60
+        return new TimeValue(newHours, newMinutes)
+    }
+}
+
+const decideWhichFieldToCalculate = (startTime, breakDuration, endTime, workHours) => {
+    const numberOfNans = 1
+}
+
+const calculateMissingTime = () => {
+    console.log("calculateMissingTime triggered")
+    const startTimeFormValue = document.getElementById("start-time").value
+    const startTime = startTimeFormValue 
+                        ? new TimeValue(...parseHoursMinutes(startTimeFormValue))
+                        : null
+
+    const breakDuration = new TimeValue(0, parseInt(document.getElementById("break-duration").value))
+
+    const endTimeFormValue = document.getElementById("end-time").value
+    const [endTimeHours, endTimeMinutes] = parseHoursMinutes(endTimeFormValue)
+    const endTime = new TimeValue(endTimeHours, endTimeMinutes)
+
+    const [workHours, workMinutes] = parseHoursMinutes(document.getElementById("work-hours").value)
+    const workTime = new TimeValue(workHours, workMinutes)
+
+    console.log("Start time:", startTime, startTime?.asHours(), startTime?.asMinutes())
+    console.log("Break:", breakDuration)
+    console.log("End time:", endTime)
+    console.log("Work hours", workTime)
+    
+    console.log("Start + break:", startTime.add(breakDuration))
+
+    //calculateEndTime([startTimeHours, startTimeMinutes], breakDuration, [workHours, workMinutes])
+
+
+}
+
+const calculateEndTime = (startTime, breakDuration, workTime) => {
+    const date = new Date()
+    console.log(date)
+    date.setHours(startTime[0])
+    date.setMinutes(startTime[1])
+    date.setSeconds(0)
+    console.log(date)
+    const interimDate = addHoursToDate(date, breakDuration / 60)
+    console.log(interimDate)
+    const finalDate = addHoursToDate(interimDate, workTime[0] + (workTime[1] / 60))
+    console.log(finalDate)
 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded")
 
-    document.getElementById("calculate-time-button").addEventListener("click", function(event) {
+    document.getElementById("calculate-time-button").addEventListener("click", (event) => {
         // console.log(event)
         event.preventDefault()
         calculateMissingTime()
@@ -34,39 +112,4 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     
 
-    const calculateMissingTime = () => {
-        console.log("calculateMissingTime triggered")
-        const startTime = document.getElementById("start-time").value
-        const [startTimeHours, startTimeMinutes] = parseHoursMinutes(startTime)
-
-        const breakDuration = parseInt(document.getElementById("break-duration").value)
-
-        const endTime = document.getElementById("end-time").value
-        const [endTimeHours, endTimeMinutes] = parseHoursMinutes(endTime)
-
-        const [workHours, workMinutes] = parseHoursMinutes(document.getElementById("work-hours").value)
-
-        console.log("Start time:", startTimeHours, startTimeMinutes)
-        console.log("Break:", breakDuration)
-        console.log("End time:", endTimeHours, endTimeMinutes)
-        console.log("Work hours",workHours, workMinutes)
-
-        calculateEndTime([startTimeHours, startTimeMinutes], breakDuration, [workHours, workMinutes])
-
-
-    }
-
-    const calculateEndTime = (startTime, breakDuration, workTime) => {
-        const date = new Date()
-        console.log(date)
-        date.setHours(startTime[0])
-        date.setMinutes(startTime[1])
-        date.setSeconds(0)
-        console.log(date)
-        const interimDate = addHoursToDate(date, breakDuration / 60)
-        console.log(interimDate)
-        const finalDate = addHoursToDate(interimDate, workTime[0] + (workTime[1] / 60))
-        console.log(finalDate)
-
-    }
 })
